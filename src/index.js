4279255118dc327366e94cf9fdbe87ff4a32f0a2
Anna Chumakova <chumakova3008@gmail.com>
2021-08-26 00:03:32 +0500

    Delete unused images

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import "./css/style.css";
 import "./js/app";
 
 import "./img/delete.png";
-import "./img/add.png";
+import "./img/add-30.png";
 import "./img/edit.png";
-import "./img/cancel.png";
-import "./img/submit.png";
